dc513e5d3929f5cadcb5fe51064251b7ecc734c1
Matthew Britt <mwbritt@hotmail.com>
2020-01-10 05:27:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,10 +11,15 @@ end
 
 def reduce(array, starting_value = nil)
   if starting_value
-    num = starting_value
+    sum = starting_value
     i = 0 
   else
-    num = array[0]
+    sum = array[0]
     i = 1 
+  end
+  while i < array.length do
+    yield(sum, array[i])
+    i += 1 
+    
   
 end
