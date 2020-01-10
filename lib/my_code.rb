680d88d91f869a161e77610a57a8b0c26041c204
Matthew Britt <mwbritt@hotmail.com>
2020-01-10 05:17:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,15 @@
 # Your Code Here
+def map(array)
+  i = 0 
+  new = []
+  while i < array.length do
+    new.push(yield(array[i]))
+    i += 1 
+  end
+  new
+end
+
+def reduce(array, starting_value = nil)
+  
+  
+end
